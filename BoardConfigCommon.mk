8aca52228948c7f943d8f7c817fc26520135c71a
sam <suofian@msn.com>
2022-04-28 07:47:55 +0200

    fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ALLOW_MISSING_DEPENDENCIES=true
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+#ALLOW_MISSING_DEPENDENCIES=true
+#BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 #SELINUX_IGNORE_NEVERALLOWS=true
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
